12835e3aac30e2b1abc88c1653ccf1aacc5779ad
hzf <1732183115@qq.com>
2024-05-26 15:34:12 +0800

    second commit

--- app/src/main/java/com/example/accountbook/MainActivity.java
+++ app/src/main/java/com/example/accountbook/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.accountbook;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Log.d("ning", "onCreate");
+
+        // 设置窗口插图监听器
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 获取按钮并设置点击监听器
+        Button buttonHelloWorld = findViewById(R.id.hello_world);
+        buttonHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("ning", "HelloWorld");
+            }
         });
     }
 }
